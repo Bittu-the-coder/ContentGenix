b76bf539f30a5cd9b709c866bb1a8167ac352ec5
Bittu-the-coder <Bittuprajapati2271@gmail.com>
2025-04-11 11:54:19 +0530

    change

--- python.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-# Replace with the actual key (if you have it)
-key = b"Y29zbW9zPQ=="  
-ciphertext = "gAAAAABlxAbc36lTjUBp7RdLdsowYQMbvrMtiRRdWpj9u4QeK7JzR_kvtEAXZ6vQAkJe4Ej3E5uXT2N2Q9NLKDh_GuH9aF-DVA=="
-
-fernet = Fernet(key)
-plaintext = fernet.decrypt(ciphertext.encode())
-print(plaintext.decode())
